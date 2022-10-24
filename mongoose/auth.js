ced9182c2f93da660bfb0957c28f558de95e526a
BhavinSonagara <81000109+BhavinSonagara@users.noreply.github.com>
2022-10-24 17:56:45 +0530

    Add files via upload

---
+++ mongoose/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const User = require("./model");
+const bcryptjs = require("bcryptjs");
+const authRouter = express.Router();
+
+// SIGN UP
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "User with same email already exists!" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    let user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = authRouter;
