432c19dc46eb5fbe471994e1bf0bc76a5f559334
AneriSonani09 <anerisonani09@gmail.com>
2022-10-25 11:30:11 +0530

    socket.io

---
+++ Router/socket/socket.io.js
@@ -0,0 +1,7 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+server.listen(3000);
