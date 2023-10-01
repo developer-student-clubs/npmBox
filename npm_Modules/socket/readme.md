9827aaafdeaa229ee04f8cdee41addd4c2695e94
Naitik Patel <naitikpatel107@gmail.com>
2023-10-01 15:33:55 +0530

    socket.io added

---
+++ npm_Modules/socket/readme.md
@@ -0,0 +1,23 @@
+# socket.io
+
+## What is socket.io ?
+
+Socket.io is a JavaScript library for real-time web communication. It enables bidirectional, event-based communication between web clients and servers, making it perfect for chat applications, live updates, and more.
+
+## Installation and Usage
+
+    npm install socket.io
+
+Use it with the server
+
+    const io = new Server(server);
+
+## Features Of socket.io
+
+- Real-time bidirectional communication.
+- Cross-browser compatibility.
+- Event-based communication.
+- Room support for grouping clients.
+- Automatic reconnection handling.
+- Middleware support for customization.
+- Socket.io is a versatile library for building real-time web applications.
--- npm_Modules/socket/socket.io.js
+++ npm_Modules/socket/socket.io.js
@@ -1,7 +1,29 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.on('connection', client => {
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
+import express from "express";
+import { Server } from "socket.io";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
-server.listen(3000);
+
+// Create a Socket.io server and attach it to the server
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Handle WebSocket connections using Socket.io
+io.on("connection", (socket) => {
+  // Listen for 'chat message' events from clients and broadcast messages to all connected clients
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
